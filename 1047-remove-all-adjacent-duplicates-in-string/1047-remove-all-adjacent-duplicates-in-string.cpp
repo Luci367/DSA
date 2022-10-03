ac0d529761cac8c5bc405c9843aae08122afbd69
Luci367 <68637984+Luci367@users.noreply.github.com>
2022-10-03 10:56:22 +0530

    Time: 56 ms (42.45%), Space: 11 MB (66.27%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -4,6 +4,7 @@ public:
         string res;
         for(char ch : s){
             if(!res.empty()&&res[res.size()-1]==ch) res.pop_back();
+            // first element else se add hoga direct due to res.empty()
             else
                 res += ch;
         }
