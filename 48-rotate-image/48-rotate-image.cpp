3b6a1bcd9d46ae5d5018c1adaa28115b919afcd2
Luci367 <68637984+Luci367@users.noreply.github.com>
2022-10-04 11:23:26 +0530

    Update 48-rotate-image.cpp

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        
+        // transpose the matrix and then reversing the columns
         int n = matrix.size();
         for(int i = 0;i<n;i++){
             for(int j =0;j<i;j++){
