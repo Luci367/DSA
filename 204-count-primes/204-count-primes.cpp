9bef9b655d6217d2a4c06ec98a4d94fc110cd82f
Luci367 <68637984+Luci367@users.noreply.github.com>
2022-10-08 16:44:56 +0530

    Time: 338 ms (42.02%), Space: 10.2 MB (68.90%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=2;i<n;i++){
             if(prime[i]){
                 count++;
-                for(int j=2*i;j<n;j+=i){
+                for(int j=i*2;j<n;j+=i){
                     prime[j] = false;
                 }
             }
